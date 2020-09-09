765778f44801fa806f75377839f84f8855cc7524
shuqian <shuqian@zhuanzhuan.com>
2020-09-09 11:39:27 +0800

    fix: 若param1或param2为null则会报错

--- src/base/utils.js
+++ src/base/utils.js
@@ -30,7 +30,7 @@ export function isCssLength(str) {
 export function isDeepEqual(param1, param2) {
   if (param1 === undefined && param2 === undefined) return true;
   else if (param1 === undefined || param2 === undefined) return false;
-  else if (param1.constructor !== param2.constructor) return false;
+  else if (param1?.constructor !== param2?.constructor) return false;
 
   if (param1.constructor === Array) {
     if (param1.length !== param2.length) return false;
